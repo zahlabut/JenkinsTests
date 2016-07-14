c45f167c5df4c5237a2bf1bb5660e624e8e109d1
zahlabut <arkadysh@gmail.com>
2016-07-14 12:00:42 +0000

    ba li meod

--- UnitestWithJenkins.py
+++ UnitestWithJenkins.py
@@ -8,14 +8,19 @@ def NV_LOG_IN_OUT(nv_url,user,password):
     driver = webdriver.Firefox()
     driver.get(nv_url)
     driver.find_element_by_name('username').send_keys(user)
+    print 'User form filled'
     time.sleep(5)
     driver.find_element_by_name('password').send_keys(password)
+    print 'Password form filled'
     time.sleep(5)
     driver.find_element_by_id('loginButton').click()
+    print 'logIn Clicked'
     time.sleep(5)
     driver.find_element_by_xpath('//*[@id="userDropdownWrapper"]/img[3]').click()
+    print 'User dropdown clicked'
     time.sleep(5)
     driver.find_element_by_xpath('//*[@id="logoutButton"]').click()
+    print 'LogOut done'
     time.sleep(5)
     #driver.quit()
 
